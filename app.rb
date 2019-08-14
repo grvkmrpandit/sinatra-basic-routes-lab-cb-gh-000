39c52ff82855cac5575ccea93d14f14627f956a4
Gaurav Pandit <grvkmrpandit@gmail.com>
2019-08-14 04:08:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/name/' do
+  get '/name' do
   "My name is Gaurav"
 end
-get '/hometown/' do
+get '/hometown' do
 "My hometown is dhanbad"
 end
-get '/favorite-song/' do
+get '/favorite-song' do
 "My favorite song is nazm"
 end
 
