1284a2bf47a7bea4c850ded2ea74ace1cb8f893f
Gaurav Pandit <grvkmrpandit@gmail.com>
2019-08-14 04:10:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Gaurav"
+  end
+  get '/hometown' do
+    "My hometown is dhanbad"
+  end
+  get '/favorite-song' do
+    "My favorite song is nazm"
+  end
+
 end
